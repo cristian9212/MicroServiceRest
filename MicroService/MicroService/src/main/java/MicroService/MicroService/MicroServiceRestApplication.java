ba9c02446a708b3d63c15294dbd5f2f527bcf801
ingta <ing.talero@gmail.com>
2024-09-14 12:07:14 -0500

    Actilizar

---
+++ MicroService/MicroService/src/main/java/MicroService/MicroService/MicroServiceRestApplication.java
@@ -0,0 +1,13 @@
+package MicroService.MicroService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceRestApplication.class, args);
+	}
+
+}
